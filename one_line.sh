ea25ba7b93d100d8e168e97c60074fa1d16290cc
web3foryou <94456349+web3foryou@users.noreply.github.com>
2021-11-22 22:28:08 +0300

    upd

--- one_line.sh
+++ one_line.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/ports_opening.sh) \
 9100
+
+. <(wget -qO- https://raw.githubusercontent.com/web3foryou/sh/main/node_exporter.sh)
