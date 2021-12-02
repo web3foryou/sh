e7c70c20328ddcc13aad920368d4d5b993fa8dd8
web3foryou <94456349+web3foryou@users.noreply.github.com>
2021-12-03 02:35:33 +0300

    upd

--- newServer/speedtest.sh
+++ newServer/speedtest.sh
@@ -3,3 +3,4 @@
 cd /root/
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt-get install speedtest
+speedtest
--- newServer/start_commands.sh
+++ newServer/start_commands.sh
@@ -1,6 +1,9 @@
 #базовые приложения - РАБОТАЕТ!
 . <(wget -qO- https://raw.githubusercontent.com/web3foryou/sh/main/newServer/fast_start.sh)
 
+#speedtest
+. <(wget -qO- https://raw.githubusercontent.com/web3foryou/sh/main/newServer/speedtest.sh)
+
 #нод экспортер для графаны - РАБОТАЕТ
 . <(wget -qO- https://raw.githubusercontent.com/web3foryou/sh/main/newServer/node_exporter.sh)
 
